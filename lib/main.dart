fd663adbf78ec799b3473584b7df93956cd0b763
Eleazar Federio <eleazarfederio@rocketmail.com>
2021-05-01 00:00:15 +0800

    taken pic preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:place_list/providers/great_places.dart';
+import 'package:place_list/screens/add_place_screen.dart';
 import 'package:place_list/screens/place_list_screen.dart';
 import 'package:provider/provider.dart';
 
@@ -19,6 +20,9 @@ class MyApp extends StatelessWidget {
           accentColor: Colors.amber
         ),
         home: PlacesListScreen(),
+        routes: {
+          AddPlaceScreen.routeName: (context) => AddPlaceScreen(),
+        },
       ),
     );
   }
